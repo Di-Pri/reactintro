c72bc204cad0085611574e635d73bf9e2854b608
Diana P <dian6210@stud.kea.dk>
2021-11-23 23:05:15 +0100

    nesting components, setting props

--- src/index.js
+++ src/index.js
@@ -2,4 +2,62 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-// ReactDOM.render(<h1>Hi Mom</h1>, document.getElementById("root"));
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Main />
+    </div>
+  );
+}
+
+function Nav() {
+  return <nav>Nav</nav>;
+}
+
+function Main() {
+  return (
+    <main>
+      <List header="ToDo" />
+      <List header="Doing" />
+      <List header="Done" />
+    </main>
+  );
+}
+
+function List(props) {
+  return (
+    <section>
+      <h2>{props.header}</h2>
+      <ul>
+        <Card />
+        <Card />
+      </ul>
+      <Form />
+    </section>
+  );
+}
+
+function Card() {
+  return (
+    <li>
+      <h3>Header</h3>
+      <Button />
+    </li>
+  );
+}
+
+function Button() {
+  return <button>Click me</button>;
+}
+
+function Form() {
+  return (
+    <form>
+      <input />
+      <Button />
+    </form>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
