51d3efcd5e21f7804e6f9b638af4926cd6b1877e
Diana P <dian6210@stud.kea.dk>
2021-11-24 00:14:15 +0100

    passing down props

--- src/App.js
+++ src/App.js
@@ -3,10 +3,28 @@ import Nav from "./components/Nav";
 import Main from "./components/Main";
 
 export default function App() {
+  const cards = [
+    {
+      title: "Make it dynamic",
+      list: "todo",
+      added: 2345345345,
+      id: 1,
+      color: "lightpink",
+      assignedTo: ["di"],
+    },
+    {
+      title: "Make it dynamic",
+      list: "todo",
+      added: 2392376395,
+      id: 2,
+      color: "lightblue",
+      assignedTo: ["di", "ma"],
+    },
+  ];
   return (
     <div className="App">
       <Nav />
-      <Main />
+      <Main cards={cards} />
     </div>
   );
 }
