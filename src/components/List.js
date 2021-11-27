cb4da0930e580932e537f6ec7e476e29cb36f5d4
Diana P <dian6210@stud.kea.dk>
2021-11-27 23:52:16 +0100

    keys

--- src/components/List.js
+++ src/components/List.js
@@ -3,7 +3,10 @@ import Card from "./Card";
 import Form from "./Form";
 
 export default function List(props) {
-  const cards = props.cards.map((card) => <Card {...card} />);
+  // When component is dynamic (created through an array), it needs a "key" property that is unique to that list
+  // so react can update things efficiently
+  // Here I am giving each Card a property called "key" and setting it's value to the ID from the data
+  const cards = props.cards.map((card) => <Card key={card.id} {...card} />);
   return (
     <section>
       <h2>{props.header}</h2>
