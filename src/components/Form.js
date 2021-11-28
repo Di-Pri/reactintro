bafc2c89d163fb9e9c6b30021b2ec04bf4f95028
Diana P <dian6210@stud.kea.dk>
2021-11-28 22:56:20 +0100

    working with forms

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,38 @@
-import React from "react";
-import Button from "./Button";
+import React, { useState } from "react";
 
 export default function Form(props) {
+  const [title, setTitle] = useState("");
+  const [color, setColor] = useState("#bada55");
   function submit(evt) {
     evt.preventDefault();
-    props.onFormSubmit("hello");
+    props.onFormSubmit({
+      title: title,
+      list: "todo",
+      added: Date.now(),
+      id: Math.random(),
+      color: color,
+      assignedTo: ["di"],
+    });
+    setTitle("");
+    setColor("#bada55");
+  }
+  function titleChanged(e) {
+    setTitle(e.target.value);
+  }
+  function colorChanged(e) {
+    setColor(e.target.value);
   }
   return (
     <form onSubmit={submit}>
-      <input />
-      <Button />
+      <label>
+        Title
+        <input type="text" onChange={titleChanged} name="title" value={title} />
+      </label>
+      <label>
+        Color
+        <input type="color" onChange={colorChanged} name="color" value={color} />
+      </label>
+      <input type="submit" value="Save" />
     </form>
   );
 }
